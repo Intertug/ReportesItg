a557615a1632628c0f62daefd4a3c9f9ddc373cc
Rubén Melo <rbmcar2@gmail.com>
2015-01-27 21:06:35 -0500

    nuevas url

--- reportes/urls.py
+++ reportes/urls.py
@@ -1,16 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from reports.views import getReportes, getDia, getMes, getDay, getReports, getMonth, getRange
+from reports.views import getDay, getReports, getMonth, getRange
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', getReportes),
-    url(r'^dia$', getDia),
-    url(r'^mes$', getMes),
     url(r'^day$', getDay),
     url(r'^month$', getMonth),
     url(r'^range$', getRange),
-    url(r'^reportes$', getReports),
+    url(r'^$', getReports),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
