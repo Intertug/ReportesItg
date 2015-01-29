1f2010a01c5faad36231cb6aa3d59548958e57a9
Ruben Melo <rbmcar2@gmail.com>
2015-01-29 09:48:07 -0500

    from urlpatterns

--- reportes/urls.py
+++ reportes/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from reports.views import getDay, getReports, getMonth, getRange
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = patterns('',
     # Examples:
