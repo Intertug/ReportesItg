ec532a1139a9bfb6622ef7ed4fa94266c1a84612
Ruben Melo <rbmcar2@gmail.com>
2015-01-30 17:52:37 -0500

    desde 0

--- reports/views.py
+++ reports/views.py
@@ -132,126 +132,17 @@ def getRange(request):
 
 def genMes(dateone, datetwo, vessel):
 
-	#ano = dateone.year
-	#if dateone.month < 10:
-	#	mes = "0" + str(dateone.month)
-	#else:
-	#	mes = dateone.month
-
 	cursor = connection.cursor()
 	cursor.execute("select TimeString, DataCode, DataValue from [2160-DAQOnBoardData] where vesselid =  "+ str(vessel) +" and TimeString > '"+str(dateone)+"' and TimeString < '"+ str(datetwo) +"' and (DataCode = 'PRP000' or DataCode = 'PRP001' or DataCode = 'PRP002' or DataCode = 'PRS000' or DataCode = 'PRS001' or DataCode = 'PRS002' or DataCode = 'BOW001' or DataCode = 'BOW002' or DataCode = 'GEP001' or DataCode = 'GEP002' or DataCode = 'GES001' or DataCode = 'GES002');")
 	#cursor.execute("EXECUTE [SP_QueryOilConsuption] "+str(vessel)+", '"+str(dateone)+"', '"+str(datetwo)+"'")
 	rows = cursor.fetchall()
 	cursor.close()
 	
-	'''
-	dias = []
+	consumo = []
 
-	for dia in range(1, 32):		
-		if dia < 10:
-			dia = "0" + str(dia)
-		else:
-			dia = str(dia)
+	for 
 
-		prp000 = []
-		prp001 = []
-		prp002 = []
-		prs000 = []
-		prs001 = []
-		prs002 = []
-		bow001 = []
-		bow002 = []
-		gep001 = []
-		gep002 = []
-		ges001 = []
-		ges002 = []
-
-		for r in rows:
-			if int(dia) < 9:
-				auxdia = "0" + str(int(dia) + 1)
-			else:
-				auxdia = int(dia) + 1
-			#print ano, mes, dia, auxdia
-
-			if str(r[0]) > (str(ano) + "-" + str(mes) + "-" + str(dia)) and str(r[0]) < (str(ano) + "-" + str(mes) + "-" + str(auxdia)):
-				if r[1] == "PRP000":
-					if r[2] > 400:
-						prp000.append(r[2])
-				elif r[1] == "PRP001":
-					prp001.append(r[2])
-				elif r[1] == "PRP002":
-					prp002.append(r[2])
-				elif r[1] == "PRS000":
-					if r[2] > 400:
-						prs000.append(r[2])
-				elif r[1] == "PRS001":
-					prs001.append(r[2])
-				elif r[1] == "PRS002":
-					prs002.append(r[2])
-				elif r[1] == "BOW001":
-					bow001.append(r[2])
-				elif r[1] == "BOW002":
-					bow002.append(r[2])
-				elif r[1] == "GEP001":
-					gep001.append(r[2])
-				elif r[1] == "GEP002":
-					gep002.append(r[2])
-				elif r[1] == "GES001":
-					ges001.append(r[2])
-				elif r[1] == "GES002":
-					ges002.append(r[2])			
-					
-		if len(prp002) == 0:
-			consumoCombustiblePropBab = 0
-		else: 
-			consumoCombustiblePropBab = round((max(prp002) - min(prp002)) * 0.2641720512415584, 2)	
-		if len(prp000) == 0:
-			consumoHorasPropBab = 0
-	 	else:
-			consumoHorasPropBab = round(len(prp000) * 0.51666 / 60.0, 2)
-
-		if len(prs002) == 0:
-			consumoCombustiblePropEst = 0
-		else:
-			consumoCombustiblePropEst = round((max(prs002) - min(prs002)) * 0.2641720512415584, 2) 
-		if len(prs000) == 0:
-			consumoHorasPropEst = 0
-		else:
-			consumoHorasPropEst = round(len(prs000) * 0.51666 / 60.0, 2)
-
-		if len(bow002) == 0:
-			consumoCombustibleBow = 0
-		else:
-			consumoCombustibleBow = round((max(bow002) - min(bow002)) * 0.2641720512415584, 2) 
-		if len(bow001) == 0:
-			consumoHorasBow = 0
-		else:
-			consumoHorasBow = round((max(bow001) - min(bow001)), 2)
-
-		if len(gep002) == 0:
-			consumoCombustibleGenBab = 0
-		else:
-			consumoCombustibleGenBab = round((max(gep002) - min(gep002)) * 0.2641720512415584, 2) 
-		if len(gep001) == 0:
-			consumoHorasGenBab = 0
-		else:
-			consumoHorasGenBab = round((max(gep001) - min(gep001)), 2)
-
-		if len(ges002) == 0:
-			consumoCombustibleGenEst = 0
-		else:
-			consumoCombustibleGenEst = round((max(ges002) - min(ges002)) * 0.2641720512415584, 2) 
-		if len(ges001) == 0:
-			consumoHorasGenEst = 0
-		else:
-			consumoHorasGenEst = round((max(ges001) - min(ges001)), 2)
-
-		total = consumoCombustiblePropBab + consumoCombustiblePropEst + consumoCombustibleBow + consumoCombustibleGenBab + consumoCombustibleGenEst
-		consumos = (consumoCombustiblePropBab, consumoHorasPropBab, consumoCombustiblePropEst, consumoHorasPropEst, consumoCombustibleBow, consumoHorasBow, consumoCombustibleGenBab, consumoHorasGenBab, consumoCombustibleGenEst, consumoHorasGenEst, total)
-
-		dias.append(consumos)							
-	'''
-	return rows
+	return consumo
 
 def genDia(dateone, tomorrow, vessel):
 
