9c80ee1bb88467030be68b1a055c1c43756ef29a
Ruben Melo <rbmcar2@gmail.com>
2015-07-16 09:40:45 -0500

    agregados Kin y tanok con cambio de horas

--- reportes/urls.py
+++ reportes/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from reports.views import ApiReporte, getDay, getReports, getMonth, getRange
+from reports.views import ApiReporte, getDay, getReports, getMonth, getDistance
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^day$', getDay),
     url(r'^month$', getMonth),
-    url(r'^range$', getRange),
+    url(r'^distance$', getDistance),
     url(r'^api$', ApiReporte),
     url(r'^$', getReports),
     # url(r'^blog/', include('blog.urls')),
--- reports/views.py
+++ reports/views.py
@@ -179,30 +179,167 @@ def getMonth(request):
 
 #Generadores de consulta
 
-def getRange(request):
+def getDistance(request):
 
-	if 'dateone' in request.GET and 'datetwo' in request.GET and 'vessel' in request.GET:
-		if(request.GET['dateone'] == '' or request.GET['datetwo'] == '' or request.GET['vessel'] == ''):
-			return render_to_response("reportesitg.html")
-		dateone = request.GET['dateone']
-		datetwo = request.GET['datetwo']
-		vessel = request.GET['vessel']
-		try:
-			#Convertimos la fechas de los input en dates de python
-			oneday = timedelta(days=1) #Creamos un delta de 1 dia
-			dateone = datetime.strptime(dateone, "%Y-%m-%d")
-			datetwo = datetime.strptime(datetwo, "%Y-%m-%d")
-			nextday = dateone+oneday #Le sumamos un dia a la fecha
-		
-			#Convertimos las fechas a string con formato AAAA-MM-DD
-			dateone = dateone.isoformat()[:10]
-			nextday = nextday.isoformat()[:10]
-			datetwo = datetwo.isoformat()[:10]
-		except ValueError:
-			raise ValueError("Incorrect data format, should be YYYY-MM-DD")
+	if 'year' in request.GET and 'month' in request.GET:
+		if(request.GET['year'] != ''):
+			year = request.GET['year']
+		else:
+			datewtime	= str(datetime.now()).split('-')
+			year		= datewtime[0]
+		if(request.GET['month'] != ''):
+			month = request.GET['month']
+		else:
+			datewtime	= str(datetime.now()).split('-')
+			month 		= datewtime[1]
+	else:
+		datewtime	= str(datetime.now()).split('-')
+		year		= datewtime[0]
+		month 		= datewtime[1]
 
-	datereporter = date.today() #Fecha en que se realizo el reporte.
-	return render_to_response("range.html", locals())
+	date = str(year)+'-'+str(month)
+
+	try:
+		#Convertimos la fechas de los input en dates de python
+		onemonth = MonthDelta(1) #Creamos un delta de 1 mes
+		dateone = datetime.strptime(date, "%Y-%m")
+		nextmonth = dateone+onemonth #Le sumamos un dia a la fecha
+	
+		#Convertimos las fechas a string con formato AAAA-MM
+		dateone = dateone.isoformat()[:7]
+		nextmonth = nextmonth.isoformat()[:7]
+	except ValueError:
+		raise ValueError("Incorrect data format, should be YYYY-MM")
+
+	if 'vessel2' in request.GET:
+		vessel = request.GET['vessel2']
+
+	if request.GET['vessel2'] in remolcadores:
+		nombre = request.GET['vessel2']
+		vessel = remolcadores[nombre]
+
+	datereporter = datetime.now();
+	vesselname = request.GET['vessel2']
+	consumo = genDistancia(dateone, vessel)
+
+	return render_to_response("distance.html", locals())
+
+def genDistancia(dateone, vessel):
+	consumos = []
+	diaUno = dateone + "-" + "01"
+	diaDos = dateone + "-" + "31"
+	oneday = timedelta(days=1) #Creamos un delta de 1 dia
+	
+	while (diaUno <= diaDos):
+
+		#Convertimos la fechas de los input en dates de python
+		diaUno = datetime.strptime(diaUno, "%Y-%m-%d")
+		tomorrow = diaUno+oneday #Le sumamos un dia a la fecha
+	
+		#Convertimos las fechas a string con formato AAAA-MM-DD
+		diaUno = diaUno.isoformat()[:10]
+		tomorrow = tomorrow.isoformat()[:10]
+
+		cursor = connection.cursor()
+		if(vessel == "Baru Pacifico" or vessel == "Baru Inti" or vessel == "Mistral" or vessel == "Vali"):
+			cursor.execute('select DataCode, DataValue from [2160-DAQOnBoardData] where vesselid =  '+ str(vessel) +' and TimeString > "'+str(dateone)+'060000" and TimeString < "'+ str(tomorrow) +'060000";')
+		else:
+			cursor.execute('select DataCode, DataValue from [2160-DAQOnBoardData] where vesselid =  '+ str(vessel) +' and TimeString > "'+str(dateone)+'" and TimeString < "'+ str(tomorrow) +'";')
+		rows = cursor.fetchall()
+		cursor.close()
+
+		prp000 = []
+		prp001 = []
+		prp002 = []
+		prs000 = []
+		prs001 = []
+		prs002 = []
+		bow001 = []
+		bow002 = []
+		gep001 = []
+		gep002 = []
+		ges001 = []
+		ges002 = []
+
+		for r in rows:
+			if r[0] == "PRP000":
+				if r[1] > 400:
+					prp000.append(r[1])
+			elif r[0] == "PRP001":
+				prp001.append(r[1])
+			elif r[0] == "PRP002":
+				prp002.append(r[1])
+			elif r[0] == "PRS000":
+				if r[1] > 400:
+					prs000.append(r[1])
+			elif r[0] == "PRS001":
+				prs001.append(r[1])
+			elif r[0] == "PRS002":
+				prs002.append(r[1])
+			elif r[0] == "BOW001":
+				bow001.append(r[1])
+			elif r[0] == "BOW002":
+				bow002.append(r[1])
+			elif r[0] == "GEP001":
+				gep001.append(r[1])
+			elif r[0] == "GEP002":
+				gep002.append(r[1])
+			elif r[0] == "GES001":
+				ges001.append(r[1])
+			elif r[0] == "GES002":
+				ges002.append(r[1])
+
+		if len(prp002) == 0:
+			consumoCombustiblePropBab = 0
+		else: 
+			consumoCombustiblePropBab = round((max(prp002) - min(prp002)) * 0.2641720512415584, 2)	
+		if len(prp000) == 0:
+			consumoHorasPropBab = 0
+	 	else:
+			consumoHorasPropBab = round(len(prp000) * 0.51666 / 60.0, 2)
+
+		if len(prs002) == 0:
+			consumoCombustiblePropEst = 0
+		else:
+			consumoCombustiblePropEst = round((max(prs002) - min(prs002)) * 0.2641720512415584, 2) 
+		if len(prs000) == 0:
+			consumoHorasPropEst = 0
+		else:
+			consumoHorasPropEst = round(len(prs000) * 0.51666 / 60.0, 2)
+
+		if len(bow002) == 0:
+			consumoCombustibleBow = 0
+		else:
+			consumoCombustibleBow = round((max(bow002) - min(bow002)) * 0.2641720512415584, 2) 
+		if len(bow001) == 0:
+			consumoHorasBow = 0
+		else:
+			consumoHorasBow = round((max(bow001) - min(bow001)), 2)
+
+		if len(gep002) == 0:
+			consumoCombustibleGenBab = 0
+		else:
+			consumoCombustibleGenBab = round((max(gep002) - min(gep002)) * 0.2641720512415584, 2) 
+		if len(gep001) == 0:
+			consumoHorasGenBab = 0
+		else:
+			consumoHorasGenBab = round((max(gep001) - min(gep001)), 2)
+
+		if len(ges002) == 0:
+			consumoCombustibleGenEst = 0
+		else:
+			consumoCombustibleGenEst = round((max(ges002) - min(ges002)) * 0.2641720512415584, 2) 
+		if len(ges001) == 0:
+			consumoHorasGenEst = 0
+		else:
+			consumoHorasGenEst = round((max(ges001) - min(ges001)), 2)
+
+		total = consumoCombustiblePropBab + consumoCombustiblePropEst + consumoCombustibleBow + consumoCombustibleGenBab + consumoCombustibleGenEst
+		consumos.append([diaUno, consumoCombustiblePropBab, consumoHorasPropBab, consumoCombustiblePropEst, consumoHorasPropEst, consumoCombustibleBow, consumoHorasBow, consumoCombustibleGenBab, consumoHorasGenBab, consumoCombustibleGenEst, consumoHorasGenEst, total])
+
+		diaUno = datetime.strptime(diaUno, "%Y-%m-%d")
+		diaUno = diaUno + oneday
+		diaUno = diaUno.isoformat()[:10]
 
 def genMes(dateone, vessel):
 
@@ -223,7 +360,10 @@ def genMes(dateone, vessel):
 		tomorrow = tomorrow.isoformat()[:10]
 
 		cursor = connection.cursor()
-		cursor.execute('select DataCode, DataValue from [2160-DAQOnBoardData] where vesselid =  '+ str(vessel) +' and TimeString > "'+str(diaUno)+'060000" and TimeString < "'+ str(tomorrow) +'060000";')
+		if(vessel == "Baru Pacifico" or vessel == "Baru Inti" or vessel == "Mistral" or vessel == "Vali"):
+			cursor.execute('select DataCode, DataValue from [2160-DAQOnBoardData] where vesselid =  '+ str(vessel) +' and TimeString > "'+str(dateone)+'060000" and TimeString < "'+ str(tomorrow) +'060000";')
+		else:
+			cursor.execute('select DataCode, DataValue from [2160-DAQOnBoardData] where vesselid =  '+ str(vessel) +' and TimeString > "'+str(dateone)+'" and TimeString < "'+ str(tomorrow) +'";')
 		rows = cursor.fetchall()
 		cursor.close()
 
@@ -325,7 +465,10 @@ def genMes(dateone, vessel):
 def genDia(dateone, tomorrow, vessel):
 
 	cursor = connection.cursor()
-	cursor.execute('select DataCode, DataValue from [2160-DAQOnBoardData] where vesselid =  '+ str(vessel) +' and TimeString > "'+str(dateone)+'060000" and TimeString < "'+ str(tomorrow) +'060000";')
+	if(vessel == "Baru Pacifico" or vessel == "Baru Inti" or vessel == "Mistral" or vessel == "Vali"):
+		cursor.execute('select DataCode, DataValue from [2160-DAQOnBoardData] where vesselid =  '+ str(vessel) +' and TimeString > "'+str(dateone)+'060000" and TimeString < "'+ str(tomorrow) +'060000";')
+	else:
+		cursor.execute('select DataCode, DataValue from [2160-DAQOnBoardData] where vesselid =  '+ str(vessel) +' and TimeString > "'+str(dateone)+'" and TimeString < "'+ str(tomorrow) +'";')
 	rows = cursor.fetchall()
 	cursor.close()
 
