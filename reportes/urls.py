a543bf569643f3f5651944f4c8e49b344a85e935
Rubén Melo <rbmcar2@gmail.com>
2015-09-30 19:40:33 -0500

    quitando lineas sobrantes

--- reportes/urls.py
+++ reportes/urls.py
@@ -1,14 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from reports.views import ApiReporte, getDay, getReports, getMonth, getDistance
+from reports.views import getDay, getReports, getMonth
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^day$', getDay),
     url(r'^month$', getMonth),
-    url(r'^distance$', getDistance),
-    url(r'^api$', ApiReporte),
     url(r'^$', getReports),
     # url(r'^blog/', include('blog.urls')),
 
